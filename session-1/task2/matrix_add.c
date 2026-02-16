f7bbef9aeaaf8ba2f275d03f2437593dbcbe9b56
josephbutcher-johns <rpkj0632@leeds.ac.uk>
2026-02-16 13:33:20 +0000

    updated

--- session-1/task2/matrix_add.c
+++ session-1/task2/matrix_add.c
@@ -10,6 +10,23 @@
     float b[4][4];
     float c[4][4];
 
+    for (int k = 0; k<4; ++k){
+        for (int j = 0; j<4; ++j){
+            a[k][j] = 1;
+            b[k][j] = 1;
+        }
+    }
+    for (int k = 0; k<4; ++k){
+        for (int j = 0; j<4; ++j){
+            c[k][j] = a[k][j] + b[k][j];
+        }
+    }
+      for (int k = 0; k<4; ++k){
+        for (int j = 0; j<4; ++j){
+            printf("%f ", c[k][j]);
+        }
+        printf("\n");
+    }
     /*
     Intialise the matrix a and b entries to 1. 
     Write code to compute the sum.
