db59d19745ce210927c4c51235628d7c7bc6a4af
josephbutcher-johns <rpkj0632@leeds.ac.uk>
2026-02-26 15:32:33 +0000

    updated

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,19 +1,38 @@
 
 /*
-Name:
-Student ID:
+Name: Joseph Butcher-Johns
+Student ID: 201948424
+Requirements:
+• The executable is run with 1-10 command line arguments
+• Each argument is 1-4 characters long
+• The output is a single string with a ‘-’ character between each argument
+For example:
+% ./makeString a b c d
+a-b-c-d
+% ./makeString helo 32 v+w
+helo-32-v+w
+Test your code in the terminal before submitting to Gradescope.
+Exam-style question
+Python accepts command–line arguments using the sys module and sys.argv parameter.
+C accepts command line arguments via the main function and argc,argv parameters.
+i. State the difference between the Python sys.argv and C argv parameters.
+ii. Explain why C requires the argc parameter and Python does not.
 */
-
 #include <stdio.h>
 #include <string.h>
 
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
-
+    char buffer[100] = ""; 
     // process the command-line data using appropriate string functions
+    for (int i = 1; i < argc; i++) {
+        strcat(buffer, argv[i]);
+        if (i < argc - 1) { 
+            strcat(buffer, "-");
+        }
+    }
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
-
+    printf("%s\n",buffer); 
     return 0;
 }
